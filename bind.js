7b7de8c110f21810cc664d488af75aa3758d5f61
Yash Tiwari <yashutiwari22@gmail.com>
2024-08-29 17:06:01 +0000

    Bind in JS

---
+++ bind.js
@@ -0,0 +1,27 @@
+// In
+/*
+class React{
+constructor() {
+    this.library = "React"
+    this.server = "https://localhost:300"
+
+    //requirement
+    document
+        .querySelector('button')
+        .addEventListener('click', this.handleClick.bind(this))
+}
+
+handleClick() {
+    console.log("button clicked");
+    console.log(this.server);
+}
+}
+
+When we used bind in handleclick call , we bind all the methods and elements 
+from the object or class and will send all those to the fucntion declaration.
+
+
+
+
+
+*/
