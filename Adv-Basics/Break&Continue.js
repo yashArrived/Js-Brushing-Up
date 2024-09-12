b5ef9d67f99731a74efde7a8dc22da422bec1b4b
Yash Tiwari <yashutiwari22@gmail.com>
2024-09-12 10:43:59 +0530

    Update Break&Continue.js

--- Adv-Basics/Break&Continue.js
+++ Adv-Basics/Break&Continue.js
@@ -4,8 +4,8 @@ break pushes the control outside the current loop's scope,
 
 
 continue skips just that 1 iteration when the condition is met.
-For ex: to print numbers from 1 to 1- and you put break when i = 5;
-then break will print till -5 , and continue will print 1,2,3,4,6,7,8,9,10,
+For ex: to print numbers from 1 to 10 and you put break when i = 5;
+then break will print till 1-5 , and continue will print 1,2,3,4,6,7,8,9,10,
 Continue will ignore 5.
 
 
