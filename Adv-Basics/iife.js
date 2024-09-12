55e12cdb6121190231484979872eebe42bafb109
Yash Tiwari <yashutiwari22@gmail.com>
2024-09-12 11:03:11 +0530

    Update iife.js

--- Adv-Basics/iife.js
+++ Adv-Basics/iife.js
@@ -21,4 +21,13 @@
 
 ((name) =>(
     console.log("connected with" , name )
-))("yash");
+))("yash
+
+  /* Benefits of IIFE
+Avoid Global Namespace Pollution: IIFE helps in preventing variable clashes in the global namespace.
+Creates Private Scope: All variables inside the IIFE are private to that function scope and not exposed to other parts of the code.
+Encapsulation: Useful when building libraries or modules where you want to expose only certain functionality.
+Executes Immediately: IIFE runs immediately after it is created, making it useful for initialization tasks.
+Drawbacks of IIFE
+Readability: Some developers find IIFE syntax harder to read, especially for newcomers to JavaScript.
+Debugging: If not named, debugging can be harder since anonymous functions don’t appear in stack traces with a name. */
